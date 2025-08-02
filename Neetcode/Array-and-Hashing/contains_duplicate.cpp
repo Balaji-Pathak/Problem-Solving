e46631be1521111de144a9473d6c5653163bed53
Balaji Pathak <balajeepathak74@gmail.com>
2025-08-02 14:46:30 +0530

    gitignore adde and file is executable

--- Neetcode/Array-and-Hashing/contains_duplicate.cpp
+++ Neetcode/Array-and-Hashing/contains_duplicate.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
 // using maping with storing frequnecy
 class Solution {
 public:
@@ -10,14 +14,23 @@ public:
     }
 };
 // using set 
-class Solution {
-public:
-    bool hasDuplicate(vector<int>& nums) {
-        unordered_set<int> set;
-        for(int num:nums){
-            if(set.count(num))return true;
-            set.insert(num);
-        }
-        return false;
-    }
-};
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+//         unordered_set<int> set;
+//         for(int num:nums){
+//             if(set.count(num))return true;
+//             set.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
+
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4};
+    bool result = sol.hasDuplicate(nums);
+    cout << (result ? "Contains duplicate" : "No duplicates") << endl;
+    return 0;
+}
