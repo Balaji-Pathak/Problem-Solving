6e9e063fc204815e71a41c1230141d3cceab334f
Balaji Pathak <balajeepathak74@gmail.com>
2025-08-02 14:48:27 +0530

    code updated

--- Neetcode/Array-and-Hashing/contains_duplicate.cpp
+++ Neetcode/Array-and-Hashing/contains_duplicate.cpp
@@ -1,33 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // using maping with storing frequnecy
-class Solution {
+class Solution
+{
 public:
-    bool hasDuplicate(vector<int>& nums) {
+    bool hasDuplicate(vector<int> &nums)
+    {
         map<int, int> freq;
-        for(auto num: nums){
+        for (auto num : nums)
+        {
             freq[num]++;
-            if(freq[num]>1) return true;
-        }return false;
+            if (freq[num] > 1)
+                return true;
+        }
+        return false;
     }
 };
-// using set 
-// class Solution {
-// public:
-//     bool hasDuplicate(vector<int>& nums) {
-//         unordered_set<int> set;
-//         for(int num:nums){
-//             if(set.count(num))return true;
-//             set.insert(num);
-//         }
-//         return false;
-//     }
-// };
+// using set
+class Solution
+{
+public:
+    bool hasDuplicate(vector<int> &nums)
+    {
+        unordered_set<int> set;
+        for (int num : nums)
+        {
+            if (set.count(num))
+                return true;
+            set.insert(num);
+        }
+        return false;
+    }
+};
 
-
-int main() {
+int main()
+{
     Solution sol;
     vector<int> nums = {1, 2, 3, 4};
     bool result = sol.hasDuplicate(nums);
