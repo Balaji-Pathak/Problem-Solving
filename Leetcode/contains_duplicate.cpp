ae20b1225483a388e9980b053e6dcea8a736e401
Balaji Pathak <balajeepathak74@gmail.com>
2025-08-02 15:01:01 +0530

    Leetcode folder added

---
+++ Leetcode/contains_duplicate.cpp
@@ -0,0 +1,45 @@
+//217. Contains Duplicate
+#include <bits/stdc++.h>
+using namespace std;
+
+// using maping with storing frequnecy
+class Solution
+{
+public:
+    bool hasDuplicate(vector<int> &nums)
+    {
+        map<int, int> freq;
+        for (auto num : nums)
+        {
+            freq[num]++;
+            if (freq[num] > 1)
+                return true;
+        }
+        return false;
+    }
+};
+// using set
+// class Solution
+// {
+// public:
+//     bool hasDuplicate(vector<int> &nums)
+//     {
+//         unordered_set<int> set;
+//         for (int num : nums)
+//         {
+//             if (set.count(num))
+//                 return true;
+//             set.insert(num);
+//         }
+//         return false;
+//     }
+// };
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 4};
+    bool result = sol.hasDuplicate(nums);
+    cout << (result ? "Contains duplicate" : "No duplicates") << endl;
+    return 0;
+}
