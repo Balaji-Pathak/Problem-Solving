0ad56009e69e9a68961bd9c36239f1bae5976e7c
Balaji Pathak <balajeepathak74@gmail.com>
2025-08-02 17:03:12 +0530

    contains duplicate II solved

--- Leetcode/contains_duplicate.cpp
+++ Leetcode/contains_duplicate.cpp
@@ -35,6 +35,32 @@ public:
 //     }
 // };
 
+
+
+
+// 219. Contains Duplicate II
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int num = nums[i];
+            if (map.count(num) && i - map[num] <= k) {
+                return true;
+            }
+            map[num] = i;
+        }
+        return false;
+
+    }
+};
+
+
+
+
+
+
 int main()
 {
     Solution sol;
